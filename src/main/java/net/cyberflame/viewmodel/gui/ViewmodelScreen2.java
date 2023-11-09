45160b7b3e3cc5d6f7e560343abacc887fefc1f3
Phyx <cimafranca76@outlook.com>
2023-11-08 23:06:04 -1000

    Initial commit

---
+++ src/main/java/net/cyberflame/viewmodel/gui/ViewmodelScreen2.java
@@ -0,0 +1,9 @@
+package net.cyberflame.viewmodel.gui;
+
+import net.minecraft.client.gui.DrawContext;
+
+public interface ViewmodelScreen2 {
+    default void renderBackground(DrawContext context) {
+    }
+
+}
